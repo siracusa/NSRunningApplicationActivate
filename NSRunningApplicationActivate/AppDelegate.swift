d11fb71d117e98bd0848b682dce8937ff19e9b77
John Siracusa <siracusa@gmail.com>
2020-02-27 16:25:29 -0500

    Bugs.

--- NSRunningApplicationActivate/AppDelegate.swift
+++ NSRunningApplicationActivate/AppDelegate.swift
@@ -22,6 +22,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    @IBAction func activateOutlook(_ sender: Any) {
+        let apps = NSWorkspace.shared.runningApplications.filter { $0.bundleIdentifier == "com.microsoft.Outlook" }
+        
+        if apps.count == 0 {
+            let alert = NSAlert()
+            alert.messageText = "Microsoft Outlook is not running."
+            alert.informativeText = "Please launch Microsoft Outlook and try the button again."
+            alert.alertStyle = .critical
+            alert.runModal()
+        }
+        else if apps.count > 1 {
+            let alert = NSAlert()
+            alert.messageText = "Found more than one instance of Microsoft Outlook."
+            alert.informativeText = "Using the first one…"
+            alert.alertStyle = .informational
+            alert.runModal()
+        }
+        else {
+            if let app = apps.first {
+                //
+                // BEGIN IMPORTANT PART
+                //
 
+                app.activate(options: [.activateAllWindows, .activateIgnoringOtherApps])
+
+                //
+                // END IMPORTANT PART
+                //
+            }
+            else {
+                let alert = NSAlert()
+                alert.messageText = "Could not get NSRunningApplication instance for Microsoft Outlook"
+                alert.informativeText = "Bleh…"
+                alert.alertStyle = .critical
+                alert.runModal()
+            }
+        }
+    }
 }
 
