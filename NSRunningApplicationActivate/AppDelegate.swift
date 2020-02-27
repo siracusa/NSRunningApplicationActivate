29e8966c0643009c2836ded2dc9fa7aa785e56ce
John Siracusa <siracusa@gmail.com>
2020-02-27 16:11:34 -0500

    Initial Commit

---
+++ NSRunningApplicationActivate/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NSRunningApplicationActivate
+//
+//  Created by John Siracusa on 2/27/20.
+//  Copyright © 2020 John Siracusa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
